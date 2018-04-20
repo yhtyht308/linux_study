211896fe8a41238f3e6aac94ff962509e21209cf
yhtyht308 <9954316@qq.com>
2018-04-21 00:13:26 +0800

    calculator2.py no run

--- 5thweek/calculator2.py
+++ 5thweek/calculator2.py
@@ -20,13 +20,32 @@ INCOME_PAYTAX_LOOKUP_TABLE=[
 	IncomePaytaxLookupItem(9000,0.25,1005),
 	IncomePaytaxLookupItem(4500,0.20,555),
 	IncomePaytaxLookupItem(1500,0.10,105),
-	IncomePaytaxLookupItem(0,0.03,0)
+IncomePaytaxLookupItem(0,0.03,0)
 ]
 
 #deal with parameters class
 class Args(object):
 	def __init__(self):
 		self.args=sys.argv[1:] 
+	
+	def _value_after_option(self,option):
+		try:
+			index=self.args.index(option)
+			return self.args[index+1]
+		except(ValueError,IndexError):
+			print('Parameter Error')
+			exit()
+	@property
+	def config_path(self):
+		return self._value_after_option('-c')
+	@property
+	def userdata_path(self):
+		return self._value_after_option('-d')
+	@property
+	def export_path(self):
+		return self._value_after_option('-o')
+
+args=Args()
 
 #configure class
 class Config(object):
@@ -34,7 +53,42 @@ class Config(object):
 		self.config=self._read_config()
 
 	def _read_conifg(self):
+		config_path=args.config_path
 		config={}
+		
+		with open(config_path) as f:
+			for line in f.readlines():
+				key,value=line.strip().split('=')
+				try:
+					config[key]=float(value)
+				except:
+					print('Parameter Error')
+					exit()
+		return config
+
+	def _get_config(self,key):
+		try:
+			return self.config
+		except KeyError:
+			print('Config Error')
+			exit()
+	@property
+	def social_insurance_baseline_low(self):
+		return self._get_config('JiShuL')
+	@property
+	def social_insurance_baseline_high(self):
+		return self._get_config('JiShuH')
+	@property
+	def social_insurance_total_rate(self):
+		return sum([
+			self._get_config('YangLao'),
+			self._get_config('YiLiao'),
+			self._get_config('ShiYe'),
+			self._get_config('GongShang'),
+			self._get_config('ShengYu'),
+			self._get_config('GongJiJin')
+		])
+config=Config()
 
 #userdata class
 class Userdata(object):
@@ -42,61 +96,68 @@ class Userdata(object):
 		self.userdata=self._read_users_data()
 
 	def  _read_users_data(self):
+		userdata_path=args.userdata_path
 		userdata=[]
+		with open userdata_path as f:
+			for line in f.readlines():
+				employee_id,income_string=line.strip.split(',')
+				try:
+					income=int(income_string)
+				except ValueError:
+					print('Parameter Error')
+					exit()
+				userdata.append((employee_id,income))
+		return userdata
+
+	def __iter__(self):
+		return iter(self.userdata)
 
 #calculator class
 class Calculator(object):
 	def __init__(self,userdata):
 		self.userdata=userdata
 
-
 #calculator social insurance function 
+	@staticmethod
 	def calculator_social_insurance(income):
-		social_insurance= income*sum(SOCIAL_INSURANCE_RATE.values())
-		return social_insurance
+		if income<config.social_insurance_baseline_low:
+			return config.social_insurance_baseline_low*config.social_insurance_total_rate
+		if income>config.social_insurance_baselin_high:
+			return config.social_insurance_baseline_high*config.social_insurance_total_rate
 
-def calculator_tax_pay(income):
-	social_insurance=calculator_social_insurance(income)
-	income_paytax=income-ORIGING_POINT-social_insurance
-	if income_paytax<=0:
-		return 0
+		return income*config.social_insurance_total_rate
+
+	@classmethod
+	def calculator_income_tax_remain(cls,income):
+		social_insurance=cls.calculator_social_insurance(income)
+		real_income=income-social_insurance
+		income_paytax=real_income-ORIGING_POINT
+		if income_paytax<=0:
+			return '0.00','{:.2f}'.format(real_income)
 	
-	for item in INCOME_PAYTAX_LOOKUP_TABLE:
-		if income_paytax > item.start_point:
-			tax_pay=income_paytax*item.tax_rate-item.quick_substractor
-#			return(format(tax_pay,".2f"))
-			return tax_pay
+		for item in INCOME_PAYTAX_LOOKUP_TABLE:
+			if income_paytax > item.start_point:
+				tax_pay=income_paytax*item.tax_rate-item.quick_substractor
+#				return (format(tax_pay,".2f")),(format(real_income-tax_pay,".2f"))
+				return '{:.2f}'.format(tax_pay),'{:.2f}'.format(real_income-tax_pay)
 
-#calculator take-home pay function
-def calculator_take_home_pay(income):
-	social_insurance=calculator_social_insurance(income)
-	tax_pay=calculator_tax_pay(income)
-	take_home_pay=income-social_insurance-tax_pay
-	return (format(take_home_pay,".2f"))
 
 	def calculator_all_userdata(self):
 		result=[]
+		for employee_id,income in self.userdata:
+			data=[employee_id,income]
+			social_insurance='{:.2f}'.format(self.calculator_social_insurance(income))
+			tax,remain=self.calculator_income_tax_remain(income)
+			data+=[social_insurance,tax,remain]
+			result.append(data)
+		return result
 
 	def export(self,default='csv')
-		
-def main():
-	if len(sys.argv)<2:
-		print("Parameter Error")
-		exit()
-#deal with parameters
-#for arg in sys.argv[1:]
-#arg.split(':')
-#char change to int and try_except 
-	for arg in sys.argv[1:]:
-		employ_id,income_string=arg.split(':')
-		try:
-			income=int(income_string)
-		except:
-			print('Parameter Error')
-			exit()
-		remain=calculator_take_home_pay(income)
-		print('{}:{}'.format(employ_id,remain))
+		result=self.calculator_all_userdata()
+		with open(args.export_path,'w',newline='') as f:
+			writer=csv.writer(f)
+			writer.writerows(result)		
 
 if __name__=='__main__':
-	main()
-
+	calculator=Calculator(Userdata())
+	calculator.export()
